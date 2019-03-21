cd8908174cea9f961d9be867c9a5abd753bd86c6
Ian Sanders <isanders@chromalloy.com>
2019-03-21 08:37:53 -0400

    Add functions for formatting and handling errors

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import util = require('util');
+import cp = require('child_process');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const promiseExec = util.promisify(cp.exec);
+
 export function activate(context: vscode.ExtensionContext) {
+    console.log("activated extension!");
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "format-python-docstrings" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+        vscode.window.showInformationMessage('Hello World!');
+    });
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+function alertError(err: cp.ExecException) {
+    if (err.message.includes("is not recognized as an internal or external command")) {
+        let alert = vscode.window.showErrorMessage("The Python module \"docformatter\" must be installed to use this extension. Would you like to install it?");
+    } else {
+        let alert = vscode.window.showErrorMessage("Unknown Error: Could not format docstrings!");
+    }
+}
+
+export function formatFile(path: string) {
+    promiseExec(`docformatter "${path}"`).then(
+        () => console.log(`Formatted dosctrings in file: "${path}"`)
+    ).catch(alertError);
+}
