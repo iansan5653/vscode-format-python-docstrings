474d33ba171fb383f46a54201336b3587f953bfe
Ian Sanders <isanders@chromalloy.com>
2019-05-22 10:21:45 -0400

    Switch to list output for Mocha
    (DevOps doesn't support indentation)

--- src/test/index.ts
+++ src/test/index.ts
@@ -17,6 +17,7 @@ import * as testRunner from 'vscode/lib/testrunner';
 // for more info
 testRunner.configure({
     ui: 'bdd',
+    reporter: 'list',
     useColors: true // colored output from test results
 });
 
